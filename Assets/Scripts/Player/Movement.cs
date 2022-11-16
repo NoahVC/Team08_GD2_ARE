64d438a1817c5f2b57eb9fc90de73bc9a1a0cc4d
CatherineSzobelDAE <55833598+CatherineSzobel@users.noreply.github.com>
2022-11-16 17:34:00 +0100

    player movement updated and added a character test scene
    the character test scene is just to test the basic movement, camera, checkpoints and climbing

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -9,9 +9,13 @@ public class Movement : MonoBehaviour
     [SerializeField] Transform orientation = null;
     [SerializeField] float walkSpeed = 4.0f;
     [SerializeField] float acceleration = 10.0f;
-
     [Header("Jumping")]
     public float jumpForce = 6f;
+
+    [Header("Climbing")]
+
+    public bool CanClimb = false;
+    public float climbSpeed = 2f;
 
     [Header("Multiplier")]
     [SerializeField] float movementMultiplier = 6f;
@@ -26,13 +30,12 @@ public class Movement : MonoBehaviour
 
 
     private Rigidbody rb;
-    private float verticalMovement;
-    private float horizontalMovement;
+    [SerializeField] float verticalMovement;
+    [SerializeField] float horizontalMovement;
     private Vector3 moveDirection;
-    private float moveSpeed = 6f;
+    [SerializeField] float moveSpeed;
     private KeyCode jumpKey = KeyCode.Space;
 
-    RaycastHit wallHit;
     // Start is called before the first frame update
     void Start()
     {
@@ -58,12 +61,51 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown(jumpKey))
         {
             if (isGrounded)
+            {
                 Jump();
+            }
         }
     }
     void Jump()
     {
         rb.velocity = Vector3.up * jumpForce;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            rb.useGravity = false;
+            Debug.Log("on trigger");
+            rb.constraints = RigidbodyConstraints.FreezePositionZ;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            CanClimb = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            rb.velocity = Vector2.up * verticalMovement * climbSpeed;
+        }
+        if (other.CompareTag("Ladder") && isGrounded)
+        {
+
+           // moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+
+            Debug.LogFormat("Grounded");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            rb.useGravity = true;
+            Debug.Log("out trigger");
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            CanClimb = false;
+        }
     }
     void MyInput()
     {
@@ -76,9 +118,13 @@ public class Movement : MonoBehaviour
         moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
 
         if (isGrounded)
+        {
             rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
         else
+        {
             rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Acceleration);
+        }
     }
     void ControlSpeed()
     {
