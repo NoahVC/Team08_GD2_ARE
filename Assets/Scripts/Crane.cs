8499857d68004b0d117c8743042837c69375fcca
giallo <114472976+gialloVero@users.noreply.github.com>
2022-11-25 22:17:11 +0100

    Created Crane and more level design

--- Assets/Scripts/Crane.cs
+++ Assets/Scripts/Crane.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bridge : ValveActivatable
+public class Crane : ValveActivatable
 {
+    private Transform _joint;
     private float _rotation;
     private float _initialRotation;
 
     private void Start()
     {
         _initialRotation = transform.rotation.eulerAngles.z;
+        _joint = transform.GetChild(0);
     }
 
     void Update()
     {
         Vector3 temp = transform.rotation.eulerAngles;
-        float change = Mathf.Clamp((_initialRotation + _rotation),275,360);
-        transform.rotation = Quaternion.Euler(temp.x, temp.y, change);
+        float change =Mathf.Clamp((_initialRotation + _rotation), -180, 90);
+        _joint.rotation = Quaternion.Euler(temp.x, change, temp.z);
+
     }
 
     public override void Activate(float rotation)
     {
         _rotation = rotation;
     }
-
 }
